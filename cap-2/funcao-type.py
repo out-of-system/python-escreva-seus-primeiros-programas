7ec58f2a85c9b77145451b57ef8f4cea0902e412
out-of-system <outofsystem753@protonmail.com>
2021-02-19 11:58:25 -0300

    funçao type

--- cap-2/funcao-type.py
+++ cap-2/funcao-type.py
@@ -11,3 +11,15 @@ isHuman = True
 print(type(name))
 print(type(age))
 print(type(isHuman))
+
+
+'''
+A função isinstance() é uma função que diz se um objeto pertence a uma classe,
+no exemplo a baixo, perguntamos para a função se a variavel 'name' é uma instancia
+da classe 'string' e ela retorna True ou False
+'''
+
+teste = isinstance(name, str)
+print(teste)
+teste2 = isinstance(name, int)
+print(teste2)
