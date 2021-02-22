d039d28808a279025787127d8a2e381d5c75b7ad
out-of-system <outofsystem753@protonmail.com>
2021-02-22 18:05:23 -0300

    upload interpolação de strings

---
+++ cap-2/interpolacao-de-string.py
@@ -0,0 +1,8 @@
+# em Python podemos interpolar strings usando % ou a função .format()
+
+minha_str = "%d dias para a copa" %(100)
+print(minha_str)
+print("%d dias para a copa 2022" %(657))
+print("faltam {} dias para a copa de 2022!".format(657))
+print("ainda faltam {dias} para o Natal!".format(dias=310))
+print("アウトシステム") # também é possivel usar o alfabeto japones em Python
