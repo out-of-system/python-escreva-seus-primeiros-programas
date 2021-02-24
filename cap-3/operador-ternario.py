2f5e5169c808c70999926d9fcc827c6375f177ac
out-of-system <outofsystem753@protonmail.com>
2021-02-24 16:03:47 -0300

    add operador ternário

---
+++ cap-3/operador-ternario.py
@@ -0,0 +1,11 @@
+imposto = 0.3
+
+print("Alto" if imposto > 0.27 else "Baixo")
+
+imposto = 0.10
+
+print("Alto" if imposto > 0.27 else "Baixo")
+
+valor_imposto = "Alto" if imposto > 0.27 else "Baixo"
+
+print(valor_imposto)
