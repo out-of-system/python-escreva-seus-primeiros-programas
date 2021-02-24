8e2fc7b6b4b2dc30cb14f0e40aad083e22ec12f1
out-of-system <outofsystem753@protonmail.com>
2021-02-24 15:02:48 -0300

    add operadores de comparação

---
+++ cap-3/operadores-de-comparacao.py
@@ -0,0 +1,28 @@
+'''
+No Python existem alguns operadores de comparações, esses operadores
+retornam True or False quando usamos alguma expresssão do tipo
+2 é maior que 1 ? False, siga alguns exemplos abaixo
+'''
+
+expressao_1 = 1 > 2 # False
+expressao_2 = 2 < 1 # False
+expressao_3 = 9 == 9 # True
+expressao_4 = 10 != 11 # True
+expressao_5 = 5 >= 5 # True
+expressao_6 = 2 <= 2 # True
+
+print(expressao_1)
+print(expressao_2)
+print(expressao_3)
+print(expressao_4)
+print(expressao_5)
+print(expressao_6)
+
+'''
+<  - Menor que
+>  - Maior que
+==  - Igual
+!=  - Não igual
+<=  - Menor ou igual que 
+>=  - Maior ou igual que
+'''
